abbc25124e89f85705572a018ee73b4282b7a918
Georgi <georgy.keranov@gmail.com>
2022-05-15 10:53:09 +0300

    Add different components

--- first-react-app/index.js
+++ first-react-app/index.js
@@ -1,15 +1,47 @@
 function Header() {
     return (
         <header>
-            <h1>This is the header</h1>
-
-            <ul>
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+            <img width="50" src="./react-logo.png"/>
+                
+            <h1>Fun facts about React</h1>
         </header>
     );
 }
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+function MainComponent() {
+    return (
+        <div>
+            <ul>
+                <li>Was first released in 2013</li>
+                
+                <li>Was originally created by Jordan Walke</li>
+                
+                <li>Has well over 100K stars on GitHub</li>
+                
+                <li>Is maintained by Facebook</li>
+                
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    );
+}
+
+function Footer() {
+    return (
+        <footer>
+            <p><small>This is the footer</small></p>
+        </footer>
+    );
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainComponent />
+            <Footer />
+        </div>
+    );
+}
+
+ReactDOM.render(<Page />, document.getElementById('root'));
