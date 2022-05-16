c4773156c7b2010c0f8585ad2d0ff98d6062729e
Georgi <georgy.keranov@gmail.com>
2022-05-16 21:46:08 +0300

    Add styles for main element

--- second-react-app/src/components/Main.js
+++ second-react-app/src/components/Main.js
@@ -3,19 +3,21 @@ import React from 'react';
 export default function Main() {
     return (
         <main className="main">
-            <h1>Fun facts about React</h1>
+            <div className="container">
+                <h1>Fun facts about React</h1>
 
-            <ul>
-                <li>Was first released in 2013</li>
-                
-                <li>Was originally created by Jordan Walke</li>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    
+                    <li>Was originally created by Jordan Walke</li>
 
-                <li>Has well over 100K stars on GitHub</li>
+                    <li>Has well over 100K stars on GitHub</li>
 
-                <li>Is maintained by Facebook</li>
-                
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
+                    <li>Is maintained by Facebook</li>
+                    
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
         </main>
     );
 }
