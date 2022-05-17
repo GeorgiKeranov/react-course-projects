3bac80d41af71669855792923f4316f4ec138757
Georgi <georgy.keranov@gmail.com>
2022-05-17 20:21:55 +0300

    Add dynamic functionality to choose random image for the meme

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import memesData from '../memesData';
 
 function Meme() {
+    const [memeImage, setMemeImage] = React.useState('');
+
+    async function generateRandomMeme() {
+        const memes = memesData.data.memes;
+        const mathRandom = Math.random();
+        const randomMemeIndex = Math.floor(mathRandom * memes.length);
+
+        const randomMeme = memes[randomMemeIndex];
+        setMemeImage(randomMeme.url);
+    }
+
     return (
         <div className="meme">
-            <form>
-                <div className="meme__input-fields">
-                    <input type="text" name="meme-top-text" placeholder="Meme top text"/>
+            <div className="meme__input-fields">
+                <input type="text" name="meme-top-text" placeholder="Meme top text"/>
 
-                    <input type="text" name="meme-bottom-text" placeholder="Meme bottom text"/>
-                </div>
+                <input type="text" name="meme-bottom-text" placeholder="Meme bottom text"/>
+            </div>
 
-                <input type="submit" value="Get a new meme image 🖼"/>
-            </form>
+            <input type="submit" onClick={generateRandomMeme} value="Get a new meme image 🖼"/>
+
+            <div className="meme__image">
+                <img src={memeImage} alt="meme" />
+            </div>
         </div>
     );
 }
