b3c178cf43635a14f5f2928a7dbf8bf8b664dda2
Georgi <georgy.keranov@gmail.com>
2022-05-18 12:00:47 +0300

    Add elements for top and bottom text and change them dynamically with the state

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -10,10 +10,9 @@ function Meme() {
 
     function generateRandomMeme() {
         const memes = memesData.data.memes;
-        const mathRandom = Math.random();
-        const randomMemeIndex = Math.floor(mathRandom * memes.length);
-
+        const randomMemeIndex = Math.floor(Math.random() * memes.length);
         const randomMeme = memes[randomMemeIndex];
+
         setMeme(prevMeme => {
             return {
                 ...prevMeme,
@@ -22,18 +21,33 @@ function Meme() {
         });
     }
 
+    function handleChange(event) {
+        const element = event.target;
+
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [element.name]: element.value
+            };
+        });
+    }
+
     return (
         <div className="meme">
             <div className="meme__input-fields">
-                <input type="text" name="meme-top-text" placeholder="Meme top text" value={meme.topText}/>
+                <input type="text" name="topText" placeholder="Meme top text" onChange={handleChange} value={meme.topText}/>
 
-                <input type="text" name="meme-bottom-text" placeholder="Meme bottom text" value={meme.bottomText}/>
+                <input type="text" name="bottomText" placeholder="Meme bottom text" onChange={handleChange} value={meme.bottomText}/>
             </div>
 
             <input type="submit" onClick={generateRandomMeme} value="Get a new meme image 🖼"/>
 
             <div className="meme__image">
-                <img src={meme.image} alt="meme" />
+                {meme.topText && <h2 className="meme__top-text">{meme.topText}</h2>}
+
+                {meme.image && <img src={meme.image} alt="meme" />}
+
+                {meme.bottomText && <h2 className="meme__bottom-text">{meme.bottomText}</h2>}
             </div>
         </div>
     );
