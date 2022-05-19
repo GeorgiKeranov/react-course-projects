d27a2976ae79aec79f5c94aea78fbd74d953bbe9
Georgi <georgy.keranov@gmail.com>
2022-05-19 11:59:50 +0300

    Add every updated note to the top of the notes

--- markdown-notes-app/App.js
+++ markdown-notes-app/App.js
@@ -7,32 +7,41 @@ import {nanoid} from "nanoid"
 export default function App() {
     const [notes, setNotes] = React.useState(() => {
         return JSON.parse(localStorage.getItem('notes')) || []
-    });
+    })
 
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
-    );
+    )
     
     // Save notes to localStorage on every change
     React.useEffect(() => {
-        localStorage.setItem('notes', JSON.stringify(notes));
-    }, [notes]);
+        localStorage.setItem('notes', JSON.stringify(notes))
+    }, [notes])
 
     function createNewNote() {
         const newNote = {
             id: nanoid(),
+            title: prompt('Please enter note title', ''),
             body: "# Type your markdown note's title here"
         }
+
         setNotes(prevNotes => [newNote, ...prevNotes])
         setCurrentNoteId(newNote.id)
     }
     
     function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
-        }))
+        setNotes(oldNotes => {
+            const noteToUpdateIndex = oldNotes.findIndex(note => note.id === currentNoteId);
+            const noteToUpdate = oldNotes[noteToUpdateIndex];
+            // Remove the note from the notes array
+            oldNotes.splice(noteToUpdateIndex, 1);
+
+            // Update the note
+            noteToUpdate.body = text;
+
+            // Return new array of notes with the updated one on the top
+            return [noteToUpdate, ...oldNotes];
+        })
     }
     
     function findCurrentNote() {
