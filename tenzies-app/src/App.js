f944d253daa25c4fdbb8a7d7052437a32833f13e
Georgi <georgy.keranov@gmail.com>
2022-05-19 12:54:07 +0300

    Add basic logic and html for the tenzies-app

--- tenzies-app/src/App.js
+++ tenzies-app/src/App.js
@@ -1,8 +1,41 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [dices, setDices] = useState(generateRandomDices(10));
+
+  function generateRandomDices(count) {
+    const randomDices = [];
+
+    for (let i = 0; i < count; i++) {
+      randomDices.push(Math.floor(Math.random() * 6) + 1);
+    }
+
+    return randomDices;
+  }
+
+  const diceElements = dices.map(dice => {
+    return (
+      <div className="dice">{dice}</div>
+    );
+  });
+
   return (
-    <h1>Hello!</h1>
+    <div className="container">
+      <div className="tenzis-game">
+        <div className="tenzis__text">
+          <h1>Tenzies</h1>
+          
+          <p>Roll until all dice are the same. Click each dice to freeze it at its current value between rolls.</p>
+        </div>
+
+        <div className="tenzis__board">
+          {dices}
+        </div>
+
+        <button className="btn">Roll</button>
+      </div>
+    </div>
   );
 }
 
